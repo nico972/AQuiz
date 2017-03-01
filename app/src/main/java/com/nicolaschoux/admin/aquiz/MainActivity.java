038112d425df7383fbff34443b476ae41035abd3
iyatan atchoro <iyatan.atchoro@mail.mcgill.ca>
2017-03-01 01:32:57 -0500

    MAin Activity done!!

--- app/src/main/java/com/nicolaschoux/admin/aquiz/MainActivity.java
+++ app/src/main/java/com/nicolaschoux/admin/aquiz/MainActivity.java
@@ -1,9 +1,29 @@
 package com.nicolaschoux.admin.aquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void make(View view){
+        Intent intent = new Intent(this, CreateQuestion.class);
+        startActivity(intent);
+
+
+    }
+
+    public void practice(View view){
+        Intent intent = new Intent(this,Practice.class);
+
+        startActivity(intent);
+
+
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/nicolaschoux/admin/aquiz/Practice.java
@@ -0,0 +1,13 @@
+package com.nicolaschoux.admin.aquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Practice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice);
+    }
+}
