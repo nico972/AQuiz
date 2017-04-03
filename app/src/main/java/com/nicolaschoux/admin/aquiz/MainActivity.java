77766f43a908d2b9afa998f2ad5b641699661877
iyatan <iyatan.atchoro@mail.mcgill.ca>
2017-04-03 16:01:41 -0400

    With buggggggggggg

--- app/src/main/java/com/nicolaschoux/admin/aquiz/MainActivity.java
+++ app/src/main/java/com/nicolaschoux/admin/aquiz/MainActivity.java
@@ -1,15 +1,19 @@
 package com.nicolaschoux.admin.aquiz;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button addQuestion;
 
     public void make(View view){
         Intent intent = new Intent(this, CreateQuestion.class);
         startActivity(intent);
+
 
 
     }
@@ -29,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addQuestion = (Button) findViewById(R.id.addq);
+        addQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+
+                View mView = getLayoutInflater().inflate(R.layout.question, null);
+
+            }
+        });
     }
 }
 //dfhksdjfhlkjshdf dflkajhsdf
